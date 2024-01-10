0db689197e4d2d3fb8899e174d84ff9ea456eb18
princekumar.2019015 <princekumar.2019015@gmail.com>
2024-01-11 05:29:43 +0530

    part-26-Add-payment-option

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
@@ -23,6 +25,7 @@ function App() {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
   };
   return (
     <BrowserRouter>
@@ -81,6 +84,7 @@ function App() {
                 element={<ShippingAddressScreen />}
               ></Route>
               <Route path="/" element={<HomeScreen />} />
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
             </Routes>
           </Container>
         </main>
